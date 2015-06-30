f77c6b87495ee25fbb8080985f29bbc9d68dd36d
Phillippe Pelzer <phil.pelzer@gmail.com>
2015-06-30 02:15:08 +0200

    update to random

--- app.js
+++ app.js
@@ -26,16 +26,10 @@ interactiveModel.prototype.init = function() {
 
   ixmodel.scene = new THREE.Scene();
   ixmodel.scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
-<<<<<<< HEAD
   ixmodel.camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, .1, 2000 );
-=======
-  ixmodel.camera = new THREE.PerspectiveCamera( 60, window.innerWidth/window.innerHeight, 0.1, 3000 );
-  
->>>>>>> origin/multiple
   ixmodel.renderer = new THREE.WebGLRenderer();
-  ixmodel.renderer.setClearColor( ixmodel.scene.fog.color );
+
   ixmodel.renderer.setSize(window.innerWidth, window.innerHeight);
-  ixmodel.renderer.setPixelRatio( window.devicePixelRatio );
 
   ixmodel.camera.position.x = 0;
   ixmodel.camera.position.y = 0;
@@ -62,7 +56,7 @@ interactiveModel.prototype.mesh = function(size, complexity) {
 
   var i;
   var geometry = new THREE.SphereGeometry( ixmodel.size, ixmodel.complexity, ixmodel.complexity );
-  var material =  new THREE.MeshLambertMaterial( { color:0xffffff, shading: THREE.FlatShading } );
+  var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 
   for (i in obj) {
     ixmodel.mesh = new THREE.Mesh(geometry, material);
@@ -72,7 +66,6 @@ interactiveModel.prototype.mesh = function(size, complexity) {
     ixmodel.mesh.updateMatrix();
     ixmodel.mesh.matrixAutoUpdate = false;
     ixmodel.textMesh = new THREEx.Text(obj[i].name);
-<<<<<<< HEAD
 	
 	ixmodel.textMesh.position.x = ( Math.random() - 0.5 ) * 1000;
 	ixmodel.textMesh.position.y = ( Math.random() - 0.5 ) * 1000;
@@ -80,11 +73,8 @@ interactiveModel.prototype.mesh = function(size, complexity) {
 
     ixmodel.titleTextMesh = new THREEx.Text('BREACH', { size: 10, height: 5 });
     ixmodel.titleTextMesh.position.set(0, 0, 50);
-=======
-    ixmodel.textMesh.position.set(random, random, random);
->>>>>>> origin/multiple
 
-    ixmodel.scene.add( ixmodel.textMesh, ixmodel.mesh );
+    ixmodel.scene.add( ixmodel.textMesh, ixmodel.mesh, ixmodel.titleTextMesh );
   }
 
   var light = new THREE.DirectionalLight( 0xffffff );
@@ -105,6 +95,7 @@ interactiveModel.prototype.render = function() {
   requestAnimationFrame(ixmodel.render);
   ixmodel.renderer.render( ixmodel.scene, ixmodel.camera );
   ixmodel.controls.update();
+  ixmodel.renderer.setClearColor( 0x000000, 1 );
   document.body.appendChild(ixmodel.renderer.domElement);
 }
 
@@ -115,7 +106,7 @@ if (document.readyState) {
   setTimeout(function() {
     ixmodel = new interactiveModel();
     ixmodel.init();
-    ixmodel.mesh(10, 100);
+    ixmodel.mesh(10, 10);
     ixmodel.render();
   }, 100);
 }
